f1039e0310c6d23b81d82f1dbac2ae1ebc5dc3aa
zhouff <sjj050121014@163.com>
2018-03-29 22:35:49 +0800

    more c code from apue

--- chapter01/myls.c
+++ chapter01/myls.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	struct dirent	*dirp;
 
 	if (argc != 2)
-		err_quit("usage: ls directory_name");
+		err_quit("usage: myls directory_name");
 
 	if ((dp = opendir(argv[1])) == NULL)
 		err_sys("can't open %s", argv[1]);
