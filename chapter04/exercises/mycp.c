278c43df767af8a8f3d9990442cbe1cdacba37f5
zhouff <sjj050121014@163.com>
2018-04-05 17:50:19 +0800

    minor changes

--- chapter04/exercises/mycp.c
+++ chapter04/exercises/mycp.c
@@ -4,18 +4,18 @@
 
 int main(int argc, char* argv[])
 {
-	int i, rfd, wfd, n;
+	int rfd, wfd, n;
 	char buf;
 	if (argc != 3)
 	{
-		printf("Usage: mycp file1 file2\2");
+		printf("Usage: mycp file1 file2\n");
 		exit(1);
 	}	
 	if ((rfd = open(argv[1], O_RDONLY)) < 0)
 		err_sys("open file1");
 	if ((wfd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, FILE_MODE)) < 0)
 		err_sys("open file2");
-	while (read(rfd, &buf, 1))
+	while ((n=read(rfd, &buf, 1))>0)
 	{
 		if (buf == '\0')  
 			continue;
