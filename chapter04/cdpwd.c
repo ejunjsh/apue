d72194e84d72eb34723b45ade5ca0c20ebbffe9d
sky <sjj050121014@163.com>
2018-04-05 17:46:18 +0800

    finish chapter04

--- chapter04/cdpwd.c
+++ chapter04/cdpwd.c
@@ -1,12 +1,18 @@
 #include "apue.h"
 
 int
-main(void)
+main(int argc, char* argv[])
 {
 	char	*ptr;
 	size_t		size;
 
-	if (chdir("/usr/spool/uucppublic") < 0)
+    if (argc != 2)
+	{
+		printf("Usage: cdpwd path\n");
+		exit(1);
+	}	
+
+	if (chdir(argv[1]) < 0)
 		err_sys("chdir failed");
 
 	ptr = path_alloc(&size);	/* our own function */
---
+++ chapter04/exercises/mycp.c
@@ -0,0 +1,29 @@
+#include "apue.h"
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	int i, rfd, wfd, n;
+	char buf;
+	if (argc != 3)
+	{
+		printf("Usage: mycp file1 file2\2");
+		exit(1);
+	}	
+	if ((rfd = open(argv[1], O_RDONLY)) < 0)
+		err_sys("open file1");
+	if ((wfd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, FILE_MODE)) < 0)
+		err_sys("open file2");
+	while (read(rfd, &buf, 1))
+	{
+		if (buf == '\0')  
+			continue;
+		if (write(wfd, &buf, 1) < 0)
+			err_sys("write");
+	}
+	if (n < 0)
+		err_sys("read");
+	
+	exit(0);
+}
