45a4703dfdfc11b79b3c0f8f39cf52186ca53406
zhouff <sjj050121014@163.com>
2018-05-02 21:49:42 +0800

    finish chapter11 exercises

---
+++ chapter11/exercises/mybarrier.c
@@ -0,0 +1,127 @@
+#include "apue.h"
+#include <pthread.h>
+#include <errno.h>
+
+typedef struct my_barrier_t {
+    pthread_mutex_t lock;
+    pthread_cond_t  cond;
+    unsigned int    count;
+    unsigned int    finished;
+} my_barrier_t;
+
+
+int
+my_barrier_wait(my_barrier_t *barrier) {
+    int ret;
+    unsigned long self = (unsigned long)pthread_self();
+    printf("%lu: about to lock on &barrier->lock\n", self);
+    pthread_mutex_lock(&barrier->lock);
+    printf("%lu: LLL: locked\n", self);
+
+    barrier->finished++;
+    printf("%lu: count=%d, finished=%d\n", self, barrier->count, barrier->finished);
+    if (barrier->count == barrier->finished) {
+        pthread_cond_broadcast(&barrier->cond);
+        printf("%lu: finished last, exiting\n", self);
+        ret = PTHREAD_BARRIER_SERIAL_THREAD;
+    } else {
+        printf("%lu: LLL about to wait on condition, and unblock the lock\n", self);
+        ret = pthread_cond_wait(&barrier->cond, &barrier->lock);
+        printf("%lu: unblocked, unblocking & exiting\n", self);
+    }
+
+    printf("%lu: LLL: about to unlock\n", self);
+    pthread_mutex_unlock(&barrier->lock);
+    return ret;
+}
+
+
+int
+my_barrier_destroy(my_barrier_t *barrier) {
+    int err;
+    printf("Destroying mutex\n");
+    err = pthread_mutex_destroy(&barrier->lock);
+    if (err != 0)
+        return err;
+    printf("Destroying condition\n");
+    err = pthread_cond_destroy(&barrier->cond);
+    if (err != 0)
+        return err;
+    return 0;
+}
+
+
+int
+my_barrier_init(my_barrier_t *barrier, void *attr, unsigned count) {
+    int err;
+    pthread_mutexattr_t mutexattr;
+
+    if (count < 1) {
+        return EINVAL;
+    }
+
+    err = pthread_mutex_init(&barrier->lock, NULL);
+    if (err != 0)
+        return err;
+
+    err = pthread_mutexattr_init(&mutexattr);
+    if (err != 0)
+        return err;
+    err = pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_ERRORCHECK);
+    if (err != 0)
+        return err;
+
+    err = pthread_cond_init(&barrier->cond, NULL);
+    if (err != 0)
+        return err;
+
+    barrier->count = count;
+    barrier->finished = 0;
+    return 0;
+}
+
+
+void *
+thread(void *arg) {
+    my_barrier_t *barrier_p = (my_barrier_t*)arg;
+    printf("%lu: starting\n", (unsigned long)pthread_self());
+    my_barrier_wait(barrier_p);
+    printf("%lu: after barrier_wait\n", (unsigned long)pthread_self());
+    return NULL;
+}
+
+
+#define NTHREADS 3
+
+
+int main() {
+    int err;
+    pthread_t threads[NTHREADS];
+
+    my_barrier_t barrier;
+    err = my_barrier_init(&barrier, NULL, NTHREADS + 1);
+    if (err != 0) {
+        err_exit(err, "Couldn't init barrier");
+    }
+
+    for (int i = 0; i < NTHREADS; ++i) {
+        err = pthread_create(&threads[i], NULL, thread, &barrier);
+        if (err != 0)
+            err_exit(err, "Can't create thread %d", i);
+    }
+
+    printf("Main thread (%lu): about to wait for barrier\n", (unsigned long)pthread_self());
+    my_barrier_wait(&barrier);
+    printf("Main thread (%lu): after wait for barrier\n", (unsigned long)pthread_self());
+
+    for (int i = 0; i < NTHREADS; ++i) {
+        err = pthread_join(threads[i], NULL);
+        if (err != 0)
+            err_exit(err, "Can't join thread %d", i);
+    }
+
+    err = my_barrier_destroy(&barrier);
+    if (err != 0) {
+        err_exit(err, "Couldn't destroy barrier");
+    }
+}
---
+++ chapter11/exercises/worker_queue.c
@@ -0,0 +1,189 @@
+#include "apue.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+struct job {
+    struct job *j_next;
+    struct job *j_prev;
+    pthread_t   j_id;   /* tells which thread handles this job */
+    /* ... more stuff here ... */
+};
+
+struct queue {
+    struct job      *q_head;
+    struct job      *q_tail;
+    pthread_rwlock_t q_lock;
+    pthread_cond_t   qready;
+    pthread_mutex_t  cond_lock;
+};
+
+/*
+ * Initialize a queue.
+ */
+int
+queue_init(struct queue *qp)
+{
+    int err;
+
+    // Initialize condition and its mutex.
+    err = pthread_cond_init(&qp->qready, NULL);
+    if (err != 0)
+        return err;
+    err = pthread_mutex_init(&qp->cond_lock, NULL);
+    if (err != 0)
+        return err;
+
+    qp->q_head = NULL;
+    qp->q_tail = NULL;
+    err = pthread_rwlock_init(&qp->q_lock, NULL);
+    if (err != 0)
+        return(err);
+    /* ... continue initialization ... */
+    return(0);
+}
+
+/*
+ * Insert a job at the head of the queue.
+ */
+void
+job_insert(struct queue *qp, struct job *jp)
+{
+    pthread_rwlock_wrlock(&qp->q_lock);
+    jp->j_next = qp->q_head;
+    jp->j_prev = NULL;
+    if (qp->q_head != NULL)
+        qp->q_head->j_prev = jp;
+    else
+        qp->q_tail = jp;    /* list was empty */
+    qp->q_head = jp;
+    pthread_rwlock_unlock(&qp->q_lock);
+    // Signal workers.
+    pthread_cond_broadcast(&qp->qready);
+}
+
+/*
+ * Append a job on the tail of the queue.
+ */
+void
+job_append(struct queue *qp, struct job *jp)
+{
+    pthread_rwlock_wrlock(&qp->q_lock);
+    jp->j_next = NULL;
+    jp->j_prev = qp->q_tail;
+    if (qp->q_tail != NULL)
+        qp->q_tail->j_next = jp;
+    else
+        qp->q_head = jp;    /* list was empty */
+    qp->q_tail = jp;
+    pthread_rwlock_unlock(&qp->q_lock);
+    // Signal workers.
+    pthread_cond_broadcast(&qp->qready);
+}
+
+/*
+ * Remove the given job from a queue.
+ */
+void
+job_remove(struct queue *qp, struct job *jp)
+{
+    pthread_rwlock_wrlock(&qp->q_lock);
+    if (jp == qp->q_head) {
+        qp->q_head = jp->j_next;
+        if (qp->q_tail == jp)
+            qp->q_tail = NULL;
+        else
+            jp->j_next->j_prev = jp->j_prev;
+    } else if (jp == qp->q_tail) {
+        qp->q_tail = jp->j_prev;
+        jp->j_prev->j_next = jp->j_next;
+    } else {
+        jp->j_prev->j_next = jp->j_next;
+        jp->j_next->j_prev = jp->j_prev;
+    }
+    pthread_rwlock_unlock(&qp->q_lock);
+}
+
+/*
+ * Find a job for the given thread ID.
+ */
+struct job *
+job_find(struct queue *qp, pthread_t id)
+{
+    struct job *jp;
+
+    if (pthread_rwlock_rdlock(&qp->q_lock) != 0)
+        return(NULL);
+
+    for (jp = qp->q_head; jp != NULL; jp = jp->j_next)
+        if (pthread_equal(jp->j_id, id))
+            break;
+
+    pthread_rwlock_unlock(&qp->q_lock);
+    return(jp);
+}
+
+
+/*
+ * Worker thread.
+ */
+void *
+worker_thread(void *arg)
+{
+    struct queue *qp = (struct queue *)arg;
+    struct job *jp;
+
+    // Wait on condition...
+    pthread_mutex_lock(&qp->cond_lock);
+    while (true) {
+        pthread_cond_wait(&qp->qready, &qp->cond_lock);
+        // When signalled, try to find a job for itself.
+        printf("Thread %lu: Was waken up..", (unsigned long)pthread_self());
+        jp = job_find(qp, pthread_self());
+        // If found, do stuff, remove job.
+        if (jp) {
+            printf("  Got a job, doing stuff..");
+            job_remove(qp, jp);
+            free(jp);
+        }
+        printf("\n");
+    }
+    pthread_mutex_unlock(&qp->cond_lock);
+}
+
+
+#define NTHREADS 3
+
+
+int main() {
+    int err;
+    pthread_t threads[NTHREADS];
+    pthread_t next_worker_tid;
+    struct queue queue;
+    struct job *jp;
+
+    srandom(time(NULL));
+
+    // Init queue.
+    err = queue_init(&queue);
+    if (err != 0)
+        err_exit(err, "can't init queue");
+
+    // Launch a bunch of threads.
+    for (int i = 0; i < NTHREADS; ++i) {
+        err = pthread_create(&threads[i], NULL, worker_thread, &queue);
+        if (err != 0)
+            err_exit(err, "can't create thread %d", i);
+    }
+
+    // Add a few jobs.
+    while (true) {
+        jp = malloc(sizeof(struct job));
+        next_worker_tid = threads[random() % NTHREADS];
+        jp->j_id = next_worker_tid;
+        printf("Master: queueing job for %lu\n", (unsigned long)next_worker_tid);
+        job_insert(&queue, jp);
+        sleep(1);
+    }
+}
