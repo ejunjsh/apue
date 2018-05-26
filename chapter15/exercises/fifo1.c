86292a92840058c4a5b92840cfc8f7c88165f530
zhouff <sjj050121014@163.com>
2018-05-26 13:04:06 +0800

    minor changes

--- chapter15/exercises/fifo1.c
+++ chapter15/exercises/fifo1.c
@@ -8,7 +8,6 @@ main(void)
 {
 	int		fdread, fdwrite;
 
-	unlink(FIFO);
 	if (mkfifo(FIFO, FILE_MODE) < 0)
 		err_sys("mkfifo error");
 	if ((fdread = open(FIFO, O_RDONLY | O_NONBLOCK)) < 0)
@@ -16,5 +15,6 @@ main(void)
 	if ((fdwrite = open(FIFO, O_WRONLY)) < 0)
 		err_sys("open error for writing");
 	clr_fl(fdread, O_NONBLOCK);
+	unlink(FIFO);
 	exit(0);
 }
