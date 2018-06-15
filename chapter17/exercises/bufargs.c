575d6b99145c83c80540ac6c1bdf39115d1abee8
zhouff <sjj050121014@163.com>
2018-06-15 22:56:52 +0800

    17.4

---
+++ chapter17/exercises/bufargs.c
@@ -0,0 +1,62 @@
+#include "apue.h"
+
+#define MAXARGC     3  /* max number of arguments in buf */
+#define WHITE   " \t\n" /* white space for tokenizing arguments */
+
+/*
+ * buf[] contains white-space-separated arguments.  We convert it to an
+ * argv-style array of pointers, and call the user's function (optfunc)
+ * to process the array.  We return -1 if there's a problem parsing buf,
+ * else we return whatever optfunc() returns.  Note that user's buf[]
+ * array is modified (nulls placed after each token).
+ */
+int
+buf_args(char *buf, int (*optfunc)(int, char **))
+{
+    char    *ptr;
+    int     argc;
+
+    int     argv_len = MAXARGC;
+    char    **argv = calloc(argv_len, sizeof(char*));
+
+    if (strtok(buf, WHITE) == NULL)     /* an argv[0] is required */
+        return(-1);
+    argv[argc = 0] = buf;
+    while ((ptr = strtok(NULL, WHITE)) != NULL) {
+        if (++argc >= argv_len-1) {    /* -1 for room for NULL at end */
+            argv_len *= 2;
+            argv = realloc(argv, argv_len * sizeof(char*));
+        }
+        argv[argc] = ptr;
+    }
+    argv[++argc] = NULL;
+
+    /*
+     * Since argv[] pointers point into the user's buf[],
+     * user's function can just copy the pointers, even
+     * though argv[] array will disappear on return.
+     */
+    int ret = ((*optfunc)(argc, argv));
+    free(argv);
+    return ret;
+}
+
+
+int print(int argc, char *argv[]) {
+    for (int i = 0; i < argc; ++i) {
+        printf("#%d: %s\n", i, argv[i]);
+    }
+    return 0;
+}
+
+
+int main() {
+    char *buf = malloc(MAXLINE);
+
+    strcpy(buf, "hello world a b c d e f g h i j k l m n o p q r s t");
+    if(buf_args(buf, print)<0){
+        err_sys("buf_args error");
+    }
+
+    return EXIT_SUCCESS;
+}
